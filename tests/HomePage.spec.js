7099fcd3a47e3d5db3a52378f6693a349a05fef8
serdarfesli <saricizmelimehmetaga@gmail.com>
2023-10-08 13:52:23 +0300

    disable github actions

--- tests/HomePage.spec.js
+++ tests/HomePage.spec.js
@@ -4,7 +4,7 @@ const { test, expect } = require('@playwright/test');
 test('Home Page', async ({ page }) => {
     await page.goto('https://www.demoblaze.com/')
 
-    const expectedPageTitle = "STOREasdasdasd"
+    const expectedPageTitle = "STORE"
     const expectedPageURL = "https://www.demoblaze.com/"
 
     await expect(page).toHaveTitle(expectedPageTitle)
