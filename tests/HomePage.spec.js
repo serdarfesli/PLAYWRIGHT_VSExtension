95cb20ebc72d018c475f4badb89c3c9b6e69eb2e
serdarfesli <saricizmelimehmetaga@gmail.com>
2023-10-08 13:43:09 +0300

    4rd commit

--- tests/HomePage.spec.js
+++ tests/HomePage.spec.js
@@ -4,7 +4,7 @@ const { test, expect } = require('@playwright/test');
 test('Home Page', async ({ page }) => {
     await page.goto('https://www.demoblaze.com/')
 
-    const expectedPageTitle = "STOREw"
+    const expectedPageTitle = "STOREasdasdasd"
     const expectedPageURL = "https://www.demoblaze.com/"
 
     await expect(page).toHaveTitle(expectedPageTitle)
