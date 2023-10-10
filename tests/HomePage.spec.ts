3585ea3b52866e4db77504b9dd5760ac33008127
serdarfesli <107787123+serdarfesli@users.noreply.github.com>
2023-10-10 19:40:10 +0300

    changed to ts extension

--- tests/HomePage.spec.ts
+++ tests/HomePage.spec.ts
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 
-test('Home Page', async ({ page }) => {
+test ('Home Page', async ({ page }) => {
     await page.goto('https://www.demoblaze.com/')
 
     const expectedPageTitle = "STORE"
