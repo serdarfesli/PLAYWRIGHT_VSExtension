3aa5d38b50e60f82d5759cca427d51f92fa5df6c
serdarfesli <107787123+serdarfesli@users.noreply.github.com>
2023-10-22 12:15:40 +0300

    2210 commit

--- tests/Built-inLocators.spec.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { test, expect } from '@playwright/test'
-
-test('built in locators', async ({ page }) => {
-await page.goto('https://dev.ofs-directory.bidout.app/')
-await page.getByRole('link', { name: 'OFS DIRECTORY', exact: true })
-
-})
-
